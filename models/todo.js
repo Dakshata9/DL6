af0ba3fae9152cafdf9103fe212392818a9bdf0f
Dakshata9 <113595710+Dakshata9@users.noreply.github.com>
2022-12-28 00:11:56 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -13,25 +13,53 @@ module.exports = (sequelize, DataTypes) => {
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-    markAsCompleted() {
-      return this.update({ completed: true });
-    }
+   
     static getTodos() {
       return this.findAll({ order: [["id", "ASC"]] });
     }
 
-    static async overdue() {
+   static async overdue() {
       return this.findAll({
         where: {
           dueDate: {
-            [Op.lt]: new Date(),
+            [Op.lt]: new Date().toLocaleDateString("en-CA"),
           },
           completed: false,
         },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueToday() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueLater() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    markAsCompleted() {
+      return this.update({ completed: true });
+    }
+    static async getTodos() {
+      return this.findAll({
+        order: [["id", "ASC"]],
       });
     }
   }
-
    
   Todo.init(
     {
